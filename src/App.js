d0f1348ce194a2a30f237f72dc3edf66073b35de
lauragonro <141372877+lauragonro@users.noreply.github.com>
2023-10-04 21:53:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,25 +8,36 @@ import {Link} from "react-router-dom";
 import Product from "./product";
 import Cart from "./cart";
 
+import Categories from "./categories";
+import Category from "./category"
+
 export default function App() {
   return (
     <div className="App">
-    <h1 className='Title'>My Fake Store :3</h1>    
-    <div className='Header'>
-      <button><Link to={"/"}>Continue shopping :3</Link></button>
-      <button><Link to={"/Cart"}>Cart</Link></button>
-      <p><User /></p>
-    </div>
-    <div className='container'>
-    <Routes>
-      <Route path="/" element={<Stock />} />
-      <Route exact path="/products/:id" element={<Product />} />
-      <Route exact path="/cart" element={<Cart />} />
-    </Routes>
-    </div>
-    <p className='Name'> Realizado por Juliana González Ronsería</p>
+      <header>
+        <h1 className='Title'>My Fake Store :3</h1>    
+        <div className='Header'>
+          <div className='Categories'>
+            <h3>Filter by categories: </h3>
+            <p><Categories /></p>
+          </div>
+          <button><Link to={"/"}>Continue shopping :3</Link></button>
+          <button><Link to={"/Cart"}>Cart</Link></button>
+          <p><User /></p>
+        </div>
+      </header>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Stock />} />
+          <Route exact path="/products/:id" element={<Product />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/category/:category" element={<Category />} />
+          <Route exact path="/category/:category/products/:id" element={<Product />} />
+        </Routes>
+      </div>
+      <footer>
+        <p className='Name'> Realizado por Juliana González Ronsería</p>
+      </footer>
     </div>
   );
 }
-
-
